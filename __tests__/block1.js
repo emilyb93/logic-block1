1ef7a7fabceab0faf90fbb992c5726b769380f6c
Emily Bennett <emily.bennett@northcoders.com>
2022-03-11 11:59:44 +0000

    formatted into an array of objects

--- __tests__/block1.js
+++ __tests__/block1.js
@@ -3,9 +3,22 @@ const { formatText } = require("../block1");
 
 describe("formatText", () => {
   test("should format the text into an an array", async () => {
-    const formattedText = await formatText();
+    const formattedArray = await formatText();
 
-    console.log(formattedText.slice(0, 10));
-    expect(Array.isArray(formattedText)).toBe(true);
+    console.log(formattedArray.slice(0, 10));
+    expect(Array.isArray(formattedArray)).toBe(true);
+  });
+
+  test("should return an array of objects with the date and text", async () => {
+    const formattedArray = await formatText();
+
+    formattedArray.forEach((dataObj) => {
+      expect(dataObj).toEqual(
+        expect.objectContaining({
+          dateTime: expect.any(String),
+          note: expect.any(String),
+        })
+      );
+    });
   });
 });
--- block1.js
+++ block1.js
@@ -1,11 +1,17 @@
+// What is the ID of the guard you chose multiplied by the minute you chose? (In the above example, the answer would be 10 * 24 = 240.)
+
 const fs = require("fs/promises");
 
 const formatText = async () => {
-  const fileInput = await fs.readFile("./input.txt", "utf8");
+  const fileInput = await fs.readFile("./test.txt", "utf8");
   //   console.log(fileInput);
   const fileInputArray = fileInput.split("\n");
+  const splitArray = fileInputArray.map((entry) => {
+    const stringEntry = entry.split(/(?<=])/);
+    return { dateTime: stringEntry[0], note: stringEntry[1] };
+  });
 
-  return fileInputArray;
+  return splitArray;
 };
 
 // formatText();
